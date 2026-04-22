87a32af465001574829ed7649fe213a627f855a7
Bittu Sharma <bittush9534@gmail.com>
2026-04-23 04:19:00 +0530

    feat: add package configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup 
+
+
+setup(
+    name="source-code-analyzer",
+    version="0.1.0",
+    author="Bittu Sharma",
+    author_email="bittush9534@gmail.com",
+    packages= find_packages(),
+    install_requires=[] 
+)
+
+
